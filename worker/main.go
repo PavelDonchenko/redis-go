7b76b96e9302255fc531002f3c2f08d36b2381e9
PavelDonchenko <przmld033@gmail.com>
2022-11-03 17:00:50 +0200

    add duration

--- worker/main.go
+++ worker/main.go
@@ -5,7 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	gomail "gopkg.in/mail.v2"
@@ -20,6 +23,7 @@ type Car struct {
 
 func main() {
 	var ctx = context.Background()
+
 	redisClient := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
@@ -31,32 +35,60 @@ func main() {
 	fmt.Println("Created subscriber")
 
 	car := Car{}
-	for {
-		message, err := subscriber.ReceiveMessage(ctx)
-		if err != nil {
-			fmt.Println("Error resive message")
-			return
-		}
 
-		err = json.Unmarshal([]byte(message.Payload), &car)
-		if err != nil {
-			return
-		}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c)
 
-		SendEmail(car)
-	}
+	ticker := time.NewTicker(time.Duration(20) * time.Second)
+	stop := make(chan bool)
+
+	go func() {
+		defer func() { stop <- true }()
+		for {
+			select {
+			case <-ticker.C:
+				message, err := subscriber.ReceiveMessage(ctx)
+				if err != nil {
+					fmt.Println("Error receive message")
+					return
+				}
+
+				err = json.Unmarshal([]byte(message.Payload), &car)
+				if err != nil {
+					return
+				}
+
+				SendEmail(car)
+			case <-stop:
+				fmt.Println("closing goroutine")
+				return
+			}
+		}
+	}()
+
+	// block until receive signal
+	<-c
+	ticker.Stop()
+
+	stop <- true
+
+	<-stop
+	fmt.Println("Stop application")
 }
 
 func SendEmail(c Car) {
+
 	m := gomail.NewMessage()
 	m.SetHeader("From", "przmld033@gmail.com")
 	m.SetHeader("To", c.Email)
 	m.SetHeader("Subject", "Gomail test subject")
-	m.SetBody("text/html", "<h2 style=\"color: red\">Your Order :</h2><h3>Car model:"+c.Model+"</h3>"+"<h3>Car year:"+strconv.Itoa(c.Year)+"</h3>"+"<h3>Car color:"+c.Color+"</h3>")
+
+	body := "<h2 style=\"color: red\">Your Order :</h2><h3>Car model:" + c.Model + "</h3>" + "<h3>Car year:" + strconv.Itoa(c.Year) + "</h3>" + "<h3>Car color:" + c.Color + "</h3>"
+	m.SetBody("text/html", body)
 
 	fmt.Println("Message created")
 
-	d := gomail.NewDialer("smtp.gmail.com", 587, "przmld033@gmail.com", "SECRET")
+	d := gomail.NewDialer("smtp.gmail.com", 587, "przmld033@gmail.com", "bsmmdmblemjtcmru")
 	//d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
 	fmt.Println("Connecting to SMTP")
